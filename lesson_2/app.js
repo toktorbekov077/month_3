445c6f980698445bf3bdaa22a41dfd5516986c24
toktorbekov077 <toktorbekov68@gmail.com>
2024-03-13 00:46:07 +0300

    frontend

---
+++ lesson_2/app.js
@@ -0,0 +1,46 @@
+//ассинхронный код
+// setTimeout(() => {
+//     console.log(1)
+// }, 2000)
+//
+// console.log(2)
+
+// const button = document.querySelector('button')
+//
+// button.addEventListener('click' , () =>{
+//     console.log(1)
+// })
+//
+// console.log(2)
+// setTimeout(() =>{
+//     console.log(3)
+// }, 200)
+// const  span = document.querySelector('span')
+// let count = 0
+//
+// const interval = setInterval(() => {
+//     count++
+//     span.innerHTML = count
+//     if (count < 60){
+//         clearInterval(interval)
+//     }
+// }, 1000)
+//
+// setTimeout(() => {
+//     clearInterval(interval)
+// } , 5000)
+
+// const  alertText = (message) => {
+//     return alert(message)
+// }
+// setTimeout(() => {
+//     alertText('hello')
+// }, 3000)
+
+//Event loop
+
+
+// Quantum
+//V8
+
+
--- project/js/home_works.js
+++ project/js/home_works.js
@@ -16,14 +16,54 @@ gmailButton.addEventListener('click' , () =>{
 
 const childBlock = document.querySelector('.child_block')
 const parentBlock = document.querySelector('.parent_block')
-let position = 0
+let positionX = 0
+let positionY = 0
+
+const maxOffsetWidth =  parentBlock.offsetWidth - childBlock.offsetWidth
+const maxOffsetHeight =  parentBlock.offsetHeight - childBlock.offsetHeight
 
 const movingBlock = () => {
-    if (position < 449){
-        position++
-        childBlock.style.left = `${position}px`
+    if (positionX < maxOffsetWidth && positionY === 0) {
+        positionX++
+        childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(movingBlock)
+    } else if (positionX >= maxOffsetWidth && positionY < maxOffsetHeight) {
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(movingBlock)
+    } else if (positionY >= maxOffsetHeight && positionX > 0) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(movingBlock)
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--
+        childBlock.style.top = `${positionY}px`
         requestAnimationFrame(movingBlock)
     }
 }
-
 movingBlock()
+
+let secondsValue = 0;
+let intervalId;
+
+function startCounter() {
+    intervalId = setInterval(() => {
+        secondsValue++;
+        document.getElementById('seconds').innerText = secondsValue;
+    }, 1000);
+}
+
+function stopCounter() {
+    clearInterval(intervalId);
+}
+
+function resetCounter() {
+    clearInterval(intervalId);
+    secondsValue = 0;
+    document.getElementById('seconds').innerText = secondsValue;
+}
+
+document.getElementById('start').addEventListener('click', startCounter);
+document.getElementById('stop').addEventListener('click', stopCounter);
+document.getElementById('reset').addEventListener('click', resetCounter);
+
